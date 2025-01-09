e6634fad391390aacd7957aebaaf06b6cae38710
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-01-08 19:38:25 -0800

    add dwm registration code

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,11 @@
 # things to run after running `home-manager switch` for the first time
 
+sudo echo "[Desktop Entry]
+Name=dwm
+Comment=Dynamic Window Manager
+Exec=dwm
+Type=Application" > /usr/share/xsessions/dwm.desktop
+
 # see https://bbs.archlinux.org/viewtopic.php?pid=2100854#p2100854
 sudo apt remove xdg-desktop-portal-gnome -y
 
