64900d27b67a4ba7a6d26286cfa7f0bbebfdb3f8
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-01-08 19:53:35 -0800

    add login shell change code

--- postinstall.sh
+++ postinstall.sh
@@ -10,6 +10,10 @@ Type=Application" | sudo tee /usr/share/xsessions/dwm.desktop
 # clone neovim config
 git clone https://github.com/LQR471814/config.nvim ~/.config/nvim
 
+# use zsh as the login shell
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+
 # see https://bbs.archlinux.org/viewtopic.php?pid=2100854#p2100854
 sudo apt remove xdg-desktop-portal-gnome -y
 
