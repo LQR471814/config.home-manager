7b2bc985cf9db5b9b8de59cdbffa8258c434a2e2
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-01-08 19:45:57 -0800

    add neovim config to postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,14 @@
 # things to run after running `home-manager switch` for the first time
 
+# register dwm as a window manager
 echo "[Desktop Entry]
 Name=dwm
 Comment=Dynamic Window Manager
 Exec=dwm
 Type=Application" | sudo tee /usr/share/xsessions/dwm.desktop
+
+# clone neovim config
+git clone https://github.com/LQR471814/config.nvim ~/.config/nvim
 
 # see https://bbs.archlinux.org/viewtopic.php?pid=2100854#p2100854
 sudo apt remove xdg-desktop-portal-gnome -y
