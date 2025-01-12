4f7c60dd69dd2fdc890fe04ab049e22615160243
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-01-11 18:20:03 -0800

    update paths

--- home_files/bin/syncthing-cfg-push.sh
+++ home_files/bin/syncthing-cfg-push.sh
@@ -1,12 +1,12 @@
 mkdir -p ~/svr-home
 sshfs "lqr471814@192.168.1.10:/home/lqr471814" ~/svr-home/
-mkdir -p ~/svr-home/config.syncthing/$(hostname)
+mkdir -p ~/svr-home/syncthing/$(hostname)
 
-cp ~/.local/state/syncthing/config.xml     ~/svr-home/config.syncthing/$(hostname)/
-cp ~/.local/state/syncthing/https-key.pem  ~/svr-home/config.syncthing/$(hostname)/
-cp ~/.local/state/syncthing/https-cert.pem ~/svr-home/config.syncthing/$(hostname)/
-cp ~/.local/state/syncthing/key.pem        ~/svr-home/config.syncthing/$(hostname)/
-cp ~/.local/state/syncthing/cert.pem       ~/svr-home/config.syncthing/$(hostname)/
+cp ~/.local/state/syncthing/config.xml     ~/svr-home/syncthing/$(hostname)/
+cp ~/.local/state/syncthing/https-key.pem  ~/svr-home/syncthing/$(hostname)/
+cp ~/.local/state/syncthing/https-cert.pem ~/svr-home/syncthing/$(hostname)/
+cp ~/.local/state/syncthing/key.pem        ~/svr-home/syncthing/$(hostname)/
+cp ~/.local/state/syncthing/cert.pem       ~/svr-home/syncthing/$(hostname)/
 
 fusermount -u ~/svr-home/
 
