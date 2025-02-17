5ca4960de6f6643e4c9f5b4ca280bcb9bb40b957
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-02-17 12:19:18 -0800

    add aw-sync to autostart

--- home_files/.dwm/autostart.sh
+++ home_files/.dwm/autostart.sh
@@ -1,5 +1,6 @@
 $HOME/.config/dwm-bar/dwm_bar.sh &
 picom --config $HOME/.config/picom/picom.conf &
 aw-qt --no-gui &
+aw-sync &
 syncthing serve --no-browser --logfile=default &
 xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
