9769f04d197b30fcefab0ddcdd2691def6969f1b
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-02-17 13:08:44 -0800

    hotfix

--- home_files/.dwm/autostart.sh
+++ home_files/.dwm/autostart.sh
@@ -1,6 +1,5 @@
 $HOME/.config/dwm-bar/dwm_bar.sh &
 picom --config $HOME/.config/picom/picom.conf &
 aw-qt --no-gui &
-aw-sync &
 syncthing serve --no-browser --logfile=default &
 xinput set-prop 10 "libinput Natural Scrolling Enabled" 1
