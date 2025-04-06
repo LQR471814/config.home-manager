b3116279bdc192b85a3c4c153efbbe657c48fb29
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-04-05 20:55:30 -0700

    add thunderbird calendar category management

---
+++ home_files/.thunderbird/user.js
@@ -0,0 +1,14 @@
+user_pref("calendar.categories.names", "Academics,Indulgence,Life Analysis,Physical Obligation,Programming,Socializing,Systems Management,Transit");
+user_pref("calendar.category.color.academics", "#ffa348");
+user_pref("calendar.category.color.analysis", "#3d3846");
+user_pref("calendar.category.color.indulgence", "#f66151");
+user_pref("calendar.category.color.life_analysis", "#3d3846");
+user_pref("calendar.category.color.physical_obligation", "#5e5c64");
+user_pref("calendar.category.color.programming", "#3584e4");
+user_pref("calendar.category.color.socializing", "#33d17a");
+user_pref("calendar.category.color.systems_management", "#663333");
+user_pref("calendar.category.color.transit", "#77767b");
+
+user_pref("calendar.alarms.eventalarmlen", 0);
+user_pref("calendar.alarms.onforevents", 1);
+user_pref("network.cookie.cookieBehavior", 2); // don't accept cookies from sites
--- src/home_files.nix
+++ src/home_files.nix
@@ -1,37 +1,67 @@
-{ pkgs, mytexlive, HOME, ... }:
+{
+  pkgs,
+  mytexlive,
+  HOME,
+  ...
+}:
 let
   DIRNAME = builtins.toString ./..;
 
   # directories in `home_files/.config/<dir>` will be symlinked to `~/.config/<dir>`
-  dotfiles = (pkgs.lib.attrsets.mapAttrs'
-    (name: value: {
+  dotfiles = (
+    pkgs.lib.attrsets.mapAttrs' (name: value: {
       name = ".config/${name}";
-      value = { source = DIRNAME + "/home_files/.config/" + name; };
-    })
-    (builtins.readDir ../home_files/.config));
+      value = {
+        source = DIRNAME + "/home_files/.config/" + name;
+      };
+    }) (builtins.readDir ../home_files/.config)
+  );
 
-  # directories in `home_files/<dir>` will be symlinked to `~/<dir>` besides `.config`
-  homefiles = (pkgs.lib.attrsets.mapAttrs'
-    (name: value: {
-      inherit name;
-      value = { source = DIRNAME + "/home_files/" + name; };
-    })
-    (pkgs.lib.attrsets.filterAttrs
-      (name: value: !(builtins.elem name [".config"]))
-      (builtins.readDir ../home_files)));
-  
+  # directories in `home_files/<dir>` will be symlinked to `~/<dir>` besides `.config` and `.thunderbird`
+  homefiles = (
+    pkgs.lib.attrsets.mapAttrs'
+      (name: value: {
+        inherit name;
+        value = {
+          source = DIRNAME + "/home_files/" + name;
+        };
+      })
+      (
+        pkgs.lib.attrsets.filterAttrs (
+          name: value:
+          !(builtins.elem name [
+            ".config"
+            ".thunderbird"
+          ])
+        ) (builtins.readDir ../home_files)
+      )
+  );
+
+  # thunderbird has special handling
+  tbdir = builtins.readDir "${HOME}/.thunderbird";
+  tbprofilename = builtins.head (
+    builtins.filter (name: builtins.match ".*\\.default" name != null) (builtins.attrNames tbdir)
+  );
+  tbfiles = {
+    ".thunderbird/${tbprofilename}/user.js" = {
+      source = DIRNAME + "/home_files/.thunderbird/user.js";
+    };
+  };
+
   texfiles = {
     texmf = {
       source = mytexlive + "/share/texmf";
     };
   };
-in
-  # `//` merges 2 attribute sets
-  dotfiles // homefiles // texfiles // {
+
+  pluginfiles = {
     ".lv2" = {
       source = "${HOME}/Production/Plugins/lv2";
     };
     ".vst3" = {
       source = "${HOME}/Production/Plugins/vst3";
     };
-  }
+  };
+in
+# `//` merges 2 attribute sets
+dotfiles // homefiles // texfiles // tbfiles // pluginfiles
