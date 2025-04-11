9eda240ec4b2c2549ba2c88652d66aa69af52427
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-04-10 21:34:39 -0700

    update categories

--- home_files/.thunderbird/user.js
+++ home_files/.thunderbird/user.js
@@ -1,4 +1,4 @@
-user_pref("calendar.categories.names", "Academics,Compulsory Event,Indulgence,Life Analysis,Physical Obligation,Programming,Socializing,Systems Management,Transit");
+user_pref("calendar.categories.names", "Academics,Compulsory Event,Indulgence,Life Analysis,Physical Obligation,Sleep,Programming,Socializing,Systems Management,Transit");
 user_pref("calendar.category.color.academics", "#ffa348");
 user_pref("calendar.category.color.analysis", "#3d3846");
 user_pref("calendar.category.color.compulsory_event", "#dc8add");
@@ -6,6 +6,7 @@ user_pref("calendar.category.color.indulgence", "#f66151");
 user_pref("calendar.category.color.life_analysis", "#3d3846");
 user_pref("calendar.category.color.physical_obligation", "#5e5c64");
 user_pref("calendar.category.color.programming", "#3584e4");
+user_pref("calendar.category.color.sleep", "#cdab8f");
 user_pref("calendar.category.color.socializing", "#33d17a");
 user_pref("calendar.category.color.systems_management", "#663333");
 user_pref("calendar.category.color.transit", "#77767b");
