9cff40360716324f168ee3f9b090338c6e221406
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-04-27 14:59:25 -0700

    update dwmblocks

--- src/derivations/dwmblocks.nix
+++ src/derivations/dwmblocks.nix
@@ -6,8 +6,8 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchFromGitHub {
     owner = "LQR471814";
     repo = "dwmblocks-async";
-    rev = "4223adbe3479f6538555a351bc8c9713fb0a8c88";
-    hash = "sha256-rK2klE6KsLWCHLODEc32+tn7d9+QWyitRD6hHjpwuYY=";
+    rev = "7abb83db049f37860579a2969925312355f4fd8e";
+    hash = "sha256-URh3QC8q7JMXm+alkCy20rMHuyIUpkB+osm6+WozTPc=";
   };
 
   prePatch = ''
