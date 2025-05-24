d570fcfe5f69f5660be7a6ed9fcb688bc9503bb1
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-05-24 12:12:57 -0700

    update mimeapps

--- src/cfg-system/mimeapps.nix
+++ src/cfg-system/mimeapps.nix
@@ -2,6 +2,20 @@
 {
   enable = true;
   defaultApplications = {
-    "application/pdf" = ["org.pwmt.zathura.desktop"];
+    "application/pdf" = [ "org.pwmt.zathura.desktop" ];
+
+    "x-scheme-handler/mailto" = [ "userapp-Thunderbird-IFAK72.desktop" ];
+    "message/rfc822" = [ "userapp-Thunderbird-IFAK72.desktop" ];
+    "x-scheme-handler/mid" = [ "userapp-Thunderbird-IFAK72.desktop" ];
+    "x-scheme-handler/news" = [ "userapp-Thunderbird-89NL72.desktop" ];
+    "x-scheme-handler/snews" = [ "userapp-Thunderbird-89NL72.desktop" ];
+    "x-scheme-handler/nntp" = [ "userapp-Thunderbird-89NL72.desktop" ];
+    "x-scheme-handler/feed" = [ "userapp-Thunderbird-0R1G72.desktop" ];
+    "application/rss+xml" = [ "userapp-Thunderbird-0R1G72.desktop" ];
+    "application/x-extension-rss" = [ "userapp-Thunderbird-0R1G72.desktop" ];
+    "x-scheme-handler/webcal" = [ "userapp-Thunderbird-GGQ262.desktop" ];
+    "text/calendar" = [ "userapp-Thunderbird-GGQ262.desktop" ];
+    "application/x-extension-ics" = [ "userapp-Thunderbird-GGQ262.desktop" ];
+    "x-scheme-handler/webcals" = [ "userapp-Thunderbird-GGQ262.desktop" ];
   };
 }
