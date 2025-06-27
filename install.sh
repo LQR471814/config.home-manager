9f6308b53e4e9445129e2ccf239508f06803b140
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-06-27 14:43:26 +0800

    add tlp

--- install.sh
+++ install.sh
@@ -143,3 +143,11 @@ Pin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref
 # disable wifi powersave
 sudo sed -i 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf && systemctl restart network-manager.service
 
+# add TLP
+
+if [[ $(hostname) == *laptop* ]]; then
+  sudo add-apt-repository ppa:linrunner/tlp
+  sudo apt update
+  sudo apt install tlp tlp-rdw
+fi
+
