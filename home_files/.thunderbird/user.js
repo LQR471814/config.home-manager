fb9fe817259bac524816d0b8a8097b3aeed16e91
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-03 14:58:07 -0700

    add category

--- home_files/.thunderbird/user.js
+++ home_files/.thunderbird/user.js
@@ -1,4 +1,4 @@
-user_pref("calendar.categories.names", "Academics,Art,Compulsory Event,Indulgence,Life Analysis,Physical Obligation,Sleep,Programming,Socializing,Systems Management,Transit");
+user_pref("calendar.categories.names", "Academics,Art,Compulsory Event,Indulgence,Life Analysis,Physical Obligation,Sleep,Programming,Socializing,Systems Management,Transit,Work");
 user_pref("calendar.category.color.academics", "#ffa348");
 user_pref("calendar.category.color.art", "#a51d2d");
 user_pref("calendar.category.color.analysis", "#3d3846");
@@ -11,6 +11,7 @@ user_pref("calendar.category.color.sleep", "#cdab8f");
 user_pref("calendar.category.color.socializing", "#33d17a");
 user_pref("calendar.category.color.systems_management", "#663333");
 user_pref("calendar.category.color.transit", "#77767b");
+user_pref("calendar.category.color.work", "#ffffff");
 
 user_pref("calendar.alarms.eventalarmlen", 0);
 user_pref("calendar.alarms.onforevents", 1);
