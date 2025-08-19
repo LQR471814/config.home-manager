ead001844179a10726aec520b5823c4044029d74
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-08-19 16:06:28 -0700

    add clipboard paste

--- src/cfg_programs/kitty.nix
+++ src/cfg_programs/kitty.nix
@@ -8,18 +8,19 @@
   font.name = "MonaspiceNe NF Light";
   settings = {
     confirm_os_window_close = 0;
-	clear_all_shortcuts = true;
+    clear_all_shortcuts = true;
   };
   keybindings = {
-	"ctrl+0" = "send_text all \\u001b[48;5u";
-	"ctrl+1" = "send_text all \\u001b[49;5u";
-	"ctrl+2" = "send_text all \\u001b[50;5u";
-	"ctrl+3" = "send_text all \\u001b[51;5u";
-	"ctrl+4" = "send_text all \\u001b[52;5u";
-	"ctrl+5" = "send_text all \\u001b[53;5u";
-	"ctrl+6" = "send_text all \\u001b[54;5u";
-	"ctrl+7" = "send_text all \\u001b[55;5u";
-	"ctrl+8" = "send_text all \\u001b[56;5u";
-	"ctrl+9" = "send_text all \\u001b[57;5u";
+    "ctrl+0" = "send_text all \\u001b[48;5u";
+    "ctrl+1" = "send_text all \\u001b[49;5u";
+    "ctrl+2" = "send_text all \\u001b[50;5u";
+    "ctrl+3" = "send_text all \\u001b[51;5u";
+    "ctrl+4" = "send_text all \\u001b[52;5u";
+    "ctrl+5" = "send_text all \\u001b[53;5u";
+    "ctrl+6" = "send_text all \\u001b[54;5u";
+    "ctrl+7" = "send_text all \\u001b[55;5u";
+    "ctrl+8" = "send_text all \\u001b[56;5u";
+    "ctrl+9" = "send_text all \\u001b[57;5u";
+    "ctrl+shift+v" = "paste_from_clipboard";
   };
 }
--- src/home_files.nix
+++ src/home_files.nix
@@ -8,18 +8,18 @@ let
   DIRNAME = builtins.toString ./..;
 
   # directories in `home_files/.config/<dir>` will be symlinked to `~/.config/<dir>`
-  dotfiles = (
-    pkgs.lib.attrsets.mapAttrs' (name: value: {
+  dotfiles =
+    (pkgs.lib.attrsets.mapAttrs' (name: value: {
       name = ".config/${name}";
       value = {
         source = DIRNAME + "/home_files/.config/" + name;
       };
-    }) (builtins.readDir ../home_files/.config)
-  ) // {
-    ".gnupg/gpg-agent.conf" = {
-      text = "pinentry-program /home/lqr471814/.nix-profile/bin/pinentry";
+    }) (builtins.readDir ../home_files/.config))
+    // {
+      ".gnupg/gpg-agent.conf" = {
+        text = "pinentry-program ${HOME}/.nix-profile/bin/pinentry";
+      };
     };
-  };
 
   # directories in `home_files/<dir>` will be symlinked to `~/<dir>` besides `.config` and `.thunderbird`
   homefiles = (
@@ -42,19 +42,21 @@ let
   );
 
   # thunderbird has special handling
-  tbfiles = if builtins.pathExists "${HOME}/.thunderbird" then
-    let
-      tbdir = builtins.readDir "${HOME}/.thunderbird";
-      tbprofilename = builtins.head (
-        builtins.filter (name: builtins.match ".*\\.default" name != null) (builtins.attrNames tbdir)
-      );
-    in
-    {
-      ".thunderbird/${tbprofilename}/user.js" = {
-        source = DIRNAME + "/home_files/.thunderbird/user.js";
-      };
-    }
-  else {};
+  tbfiles =
+    if builtins.pathExists "${HOME}/.thunderbird" then
+      let
+        tbdir = builtins.readDir "${HOME}/.thunderbird";
+        tbprofilename = builtins.head (
+          builtins.filter (name: builtins.match ".*\\.default" name != null) (builtins.attrNames tbdir)
+        );
+      in
+      {
+        ".thunderbird/${tbprofilename}/user.js" = {
+          source = DIRNAME + "/home_files/.thunderbird/user.js";
+        };
+      }
+    else
+      { };
 
   texfiles = {
     texmf = {
@@ -62,14 +64,21 @@ let
     };
   };
 
-  pluginfiles = if (builtins.pathExists "${HOME}/Production/Plugins/lv2") && (builtins.pathExists "${HOME}/Production/Plugins/vst3") then {
-    ".lv2" = {
-      source = "${HOME}/Production/Plugins/lv2";
-    };
-    ".vst3" = {
-      source = "${HOME}/Production/Plugins/vst3";
-    };
-  } else {};
+  pluginfiles =
+    if
+      (builtins.pathExists "${HOME}/Production/Plugins/lv2")
+      && (builtins.pathExists "${HOME}/Production/Plugins/vst3")
+    then
+      {
+        ".lv2" = {
+          source = "${HOME}/Production/Plugins/lv2";
+        };
+        ".vst3" = {
+          source = "${HOME}/Production/Plugins/vst3";
+        };
+      }
+    else
+      { };
 in
 # `//` merges 2 attribute sets
 dotfiles // homefiles // texfiles // tbfiles // pluginfiles
