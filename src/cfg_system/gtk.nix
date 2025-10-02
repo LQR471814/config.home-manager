cc5ab562d0adf5769fa7b6d858523a2b880b8284
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2025-10-02 16:27:23 -0700

    feat: add more shortcut directories

--- src/cfg_system/gtk.nix
+++ src/cfg_system/gtk.nix
@@ -6,8 +6,11 @@
     package = pkgs.papirus-icon-theme;
   };
   gtk3.bookmarks = [
+    "file:///home/lqr471814/Downloads"
     "file:///home/lqr471814/files/Documents/Notes"
     "file:///home/lqr471814/files/Books"
+    "file:///home/lqr471814/files/Music"
+    "file:///home/lqr471814/files/Scores"
     "file:///home/lqr471814/files/Documents"
     "file:///home/lqr471814/files"
   ];
