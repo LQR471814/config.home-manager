bc87d5ece241ef218bd78e7bb7e937e97fe6b845
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-02 15:49:29 -0800

    feat: add kanshi laptop docked profile

--- src/cfg_system/kanshi.nix
+++ src/cfg_system/kanshi.nix
@@ -36,5 +36,30 @@
         }
       ];
     }
+    {
+      profile.name = "laptop-docked";
+      profile.outputs = [
+        {
+          criteria = "eDP-1";
+          status = "disable";
+        }
+        {
+          criteria = "Dell Inc. DELL S2725QS 21SDT84";
+          status = "enable";
+          mode = "3840x2160@60.000000Hz";
+          scale = 2.0;
+          adaptiveSync = false;
+          position = "0,0";
+        }
+        {
+          criteria = "LG Electronics 27GL650F 910NTDV3G842";
+          status = "enable";
+          mode = "1920x1080@120.000000Hz";
+          scale = 1.0;
+          adaptiveSync = false;
+          position = "1920,0";
+        }
+      ];
+    }
   ];
 }
