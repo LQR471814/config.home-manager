3e9e1fa0536c28a9c890abb0d5dccce132fec519
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-21 20:18:35 -0800

    feat: update batcharge

--- src/cfg_programs/fish.nix
+++ src/cfg_programs/fish.nix
@@ -8,7 +8,7 @@
     nvt = "tmux-spawn $PWD 'nvim .'";
     shut = "sudo shutdown now -h";
     batchargefull = "sudo tlp setcharge 0 100";
-    batcharge70 = "sudo tlp setcharge 0 70";
+    batcharge80 = "sudo tlp setcharge 0 80";
     batcharge40 = "sudo tlp setcharge 40 60";
     batcharge50 = "sudo tlp setcharge 40 50";
     batreset = "sudo tlp start";
