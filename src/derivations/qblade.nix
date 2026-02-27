0f24e8a313cc21da8d6be52c1417d7b7a0e1d4c0
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-02-27 15:23:49 -0800

    feat: qbladece

---
+++ src/derivations/qblade.nix
@@ -0,0 +1,37 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+let
+  libs = with pkgs; [
+    libGL
+    libGLU
+    libsForQt5.qt5.qtbase
+  ];
+  libPath = pkgs.lib.makeLibraryPath libs;
+in
+pkgs.stdenv.mkDerivation {
+  name = "qblade";
+
+  src = pkgs.fetchurl {
+    url = "https://qblade.org/assets/QBladeCE_2.0.9.6_unix.zip";
+    hash = "sha256-05J1uFwUNevjHLfPmjdZ1MRyq69LEFvhZbJuBcQJrIo=";
+  };
+
+  nativeBuildInputs = with pkgs; [
+    unzip
+  ];
+  buildInputs = libs;
+
+  dontWrapQtApps = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r . $out
+    mv $out/QBladeCE* $out/bin/qbladece
+  '';
+
+  preFixup = ''
+    patchelf --set-rpath "$(patchelf --print-rpath $out/bin/qbladece):$out/Libraries:${libPath}" $out/bin/qbladece
+  '';
+}
