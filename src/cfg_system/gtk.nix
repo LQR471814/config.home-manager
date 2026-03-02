ebf118fe413e389fe96aff55ffd190401007d959
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-01 18:30:57 -0800

    update: file shortcuts

--- src/cfg_system/gtk.nix
+++ src/cfg_system/gtk.nix
@@ -7,17 +7,12 @@
   };
   gtk3.bookmarks = [
     "file:///home/lqr471814/Downloads"
-    "file:///home/lqr471814/files/Documents/Notes"
-    "file:///home/lqr471814/files/Documents/Notes/Recruiting/_Information/Resume"
+    "file:///home/lqr471814/files/Documents"
+    "file:///home/lqr471814/files/Documents/School"
+    "file:///home/lqr471814/files/Documents/Knowledge Base"
+    "file:///home/lqr471814/files/Documents/Recruiting"
     "file:///home/lqr471814/files/Books"
     "file:///home/lqr471814/files/Music"
-    "file:///home/lqr471814/files/Scores"
-    "file:///home/lqr471814/files/Automa"
-    "file:///home/lqr471814/files/Documents/FreeCAD"
-    "file:///home/lqr471814/files/Documents/Blender"
-    "file:///home/lqr471814/files/Documents/MuseScore4"
-    "file:///home/lqr471814/files/Plugins"
-    "file:///home/lqr471814/files/Documents"
     "file:///home/lqr471814/files"
   ];
 }
