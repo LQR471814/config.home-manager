f0710ab8bd41cce0a33619ae6994067f838bb689
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-03-02 15:29:16 -0800

    feat: update yazi paths

--- src/derivations/qblade.nix
+++ src/derivations/qblade.nix
@@ -5,6 +5,8 @@
   stdenv,
   fetchurl,
   lib,
+  makeWrapper,
+  writeShellScriptBin,
 
   unzip,
   libGL,
@@ -17,30 +19,39 @@ let
     libGLU
     libsForQt5.qt5.qtbase
   ];
+
   libPath = lib.makeLibraryPath libs;
-in
-stdenv.mkDerivation {
-  name = "qblade";
+  qblade = stdenv.mkDerivation {
+    name = "qblade";
 
-  src = fetchurl {
-    url = "https://qblade.org/assets/QBladeCE_2.0.9.6_unix.zip";
-    hash = "sha256-05J1uFwUNevjHLfPmjdZ1MRyq69LEFvhZbJuBcQJrIo=";
+    src = fetchurl {
+      url = "https://qblade.org/assets/QBladeCE_2.0.9.6_unix.zip";
+      hash = "sha256-05J1uFwUNevjHLfPmjdZ1MRyq69LEFvhZbJuBcQJrIo=";
+    };
+
+    nativeBuildInputs = [
+      unzip
+      makeWrapper
+    ];
+    buildInputs = libs;
+
+    dontWrapQtApps = true;
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r . $out
+      mv $out/QBladeCE* $out/bin/qbladece
+    '';
+
+    preFixup = ''
+      patchelf --set-rpath "$(patchelf --print-rpath $out/bin/qbladece):$out/Libraries:${libPath}" $out/bin/qbladece
+    '';
   };
-
-  nativeBuildInputs = [
-    unzip
-  ];
-  buildInputs = libs;
-
-  dontWrapQtApps = true;
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r . $out
-    mv $out/QBladeCE* $out/bin/qbladece
-  '';
-
-  preFixup = ''
-    patchelf --set-rpath "$(patchelf --print-rpath $out/bin/qbladece):$out/Libraries:${libPath}" $out/bin/qbladece
-  '';
-}
+in
+writeShellScriptBin "qblade" ''
+  APP_DIR="$HOME/.local/share/qblade"
+  mkdir -p "$APP_DIR"
+  cp -f ${qblade}/bin/qbladece "$APP_DIR/qbladece"
+  ln -s ${qblade}/ControllerFiles "$APP_DIR/ControllerFiles"
+  exec "$APP_DIR/qbladece" "$@"
+''
