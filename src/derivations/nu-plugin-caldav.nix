54be776fb0cfbf669c08cfc42a00aad84e6659bf
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-04-04 08:40:15 -0700

    feat: update nu_plugin_caldav code

--- src/derivations/nu-plugin-caldav.nix
+++ src/derivations/nu-plugin-caldav.nix
@@ -13,8 +13,8 @@ buildGoModule {
   src = fetchFromGitHub {
     owner = "lqr471814";
     repo = "nu_plugin_caldav";
-    rev = "f978c63d44f320cafbd14e3b547f1f5c5389e463";
-    hash = "sha256-OlzTEOVoZ+rpjL/l0HkbExJXqrH+ZfYRqZbOZMq0uIk=";
+    rev = "c0db0889bab26b8af2c51ad19cc7ec25d2f9ae84";
+    hash = "sha256-pZVBc4tmMXVRVZ83pmCNMO0/RzB6ehLsjWUcrVMDIoA=";
   };
 
   vendorHash = "sha256-vDt69hE7ZgAp1CdNSibRdpUvUpG575hOzfe7Sqs0RBY=";
