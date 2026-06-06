0334b49bbc26e99b2039aca7f6f9189ace1e0295
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-06 13:21:30 -0700

    refactor(kitty): adjust font size

--- src/cfg-programs/kitty.nix
+++ src/cfg-programs/kitty.nix
@@ -5,6 +5,7 @@
   themeFile = "Catppuccin-Mocha";
   font.package = pkgs.nerd-fonts.lilex;
   font.name = "Lilex Nerd Font";
+  font.size = 12;
   settings = {
     confirm_os_window_close = 0;
     clear_all_shortcuts = true;
