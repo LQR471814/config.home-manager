63cfb43149d3b643546cc08e37dde8c1da508725
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-06-07 21:01:21 -0700

    feat(daw): plugins

--- src/package-sets/gui/design-software.nix
+++ src/package-sets/gui/design-software.nix
@@ -5,17 +5,10 @@
 }:
 with pkgs;
 [
-  ardour
-  (fix-pw musescore)
-  (fix-pw easyeffects)
   blender
   kdePackages.kdenlive
   gimp3
   inkscape
   scribus
   freecad
-
-  linvstmanager
-  pwvucontrol # audio patcher
-  qpwgraph
 ]
---
+++ src/package-sets/gui/music.nix
@@ -0,0 +1,42 @@
+{
+  pkgs ? import <nixpkgs> { },
+  fix-pw,
+  ...
+}:
+with pkgs;
+[
+  ardour
+  (fix-pw musescore)
+  (fix-pw easyeffects)
+
+  linvstmanager
+  pwvucontrol # audio patcher
+  qpwgraph
+
+  # analyzers
+  lsp-plugins
+  x42-plugins
+
+  # drums
+  geonkick
+  drumgizmo
+  x42-avldrums
+
+  # effects
+  aether-lv2
+  airwindows-lv2
+  bankstown-lv2
+  bolliedelayxt-lv2
+  bs2b-lv2
+
+  # samplers
+  sfizz-ui
+  decent-sampler
+
+  # synthesizers
+  x42-gmsynth
+  vital
+  dexed
+  odin2
+  surge-xt
+]
