6213fdb5343cbfc1f4e62752e4f6c58b8dfaeba2
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-08-07 20:24:22 -0700

    feat(nushell): add wait-for back

--- src/cfg-programs/nushell.nix
+++ src/cfg-programs/nushell.nix
@@ -38,6 +38,7 @@ in
     ndev = "nix develop --command fish";
     sbtop = "sudo (which btop | get 0.path)";
     rm = "nu ${../../home-files/bin/safe-rm.nu}";
+    wait-for = "systemd-inhibit --what idle --mode block --who $env.USER --why \"Manual idle inhibit\"";
   };
   plugins = [
     # nushellPlugins.polars
